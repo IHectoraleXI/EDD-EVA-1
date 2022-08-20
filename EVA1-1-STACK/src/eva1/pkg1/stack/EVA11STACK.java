3148ecf9a80e751fd7902f41f485fdab60f079a1
IHectoraleXI <104407877+IHectoraleXI@users.noreply.github.com>
2022-08-20 10:17:04 -0600

    Add files via upload

---
+++ EVA1-1-STACK/src/eva1/pkg1/stack/EVA11STACK.java
@@ -0,0 +1,15 @@
+package eva1.pkg1.stack;
+
+public class EVA11STACK {
+
+    public static void main(String[] args) {
+        A();
+    }
+    public static void A(){
+        B();
+    }
+    public static void B(){
+        int i = 1;
+        double d = 10.0;
+    }
+}
